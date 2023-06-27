f923f00ff6367de4403b0ec0f07a267be23fbd3f
JKD10 <jonathan.moya.y@gmail.com>
2023-06-26 21:00:56 -0400

    Se modifica y ordena el codigo

--- sprintFinal2/src/sprintFinal2/Asesoria.java
+++ sprintFinal2/src/sprintFinal2/Asesoria.java
@@ -3,12 +3,9 @@ package sprintFinal2;
 import java.util.List;
 
 public interface Asesoria {
-    public void analizarUsuario();
-    
-    public void listarUsuario();
-    
+    public void analizarUsuario();    
+    public void listarUsuario();    
     public List<Capacitacion> getCapacitaciones();
-
     public String getNombreCompleto();
 }
 
